10224b86e80dbaae42ccc9926b6296c2fdebe262
Olson <colson2275@gmail.com>
2021-05-14 11:08:11 -0400

    add root reducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import Routes from "./routes";
+
+import reducers from "./store/reducers";
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+
+ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
+    <BhxBrowser>
+        <Routes/>
+    </BhxBrowser>
+</Provider>, document.getElementById("root"));
