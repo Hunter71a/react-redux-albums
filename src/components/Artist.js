e7e8d8f6ce899ceab030a5f963c28284f6ee134f
Carl Olson <colson9@student.cscc.edu>
2021-05-30 13:59:12 -0400

    update artist.js

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,46 +1,37 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {artistDetail} from '../store/actions';
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { artistDetail } from '../store/actions';
 
 class Artist extends Component {
+  componentDidMount() {
+    this.props.dispatch(artistDetail(this.props.match.params.id));
+  }
 
-componentDidMount() {
-    this.props.dispatch(artistDetail(this.props.match.params.id))
+  artistTemplate = (data) =>
+    data.artistData ? (
+      <div className="artist_view">
+        <div
+          className="artist_background"
+          style={{
+            background: `url(/images/${data.artistData[0].cover})`,
+          }}
+        >
+          <Link>Back Home</Link>
+        </div>
+      </div>
+    ) : null;
+
+  render() {
+    console.log(this.props);
+    return <>{this.artistTemplate(this.props.artistData)}</>;
+  }
 }
 
-artistTemplate = (data) => (
-    data.artistData ?
-    <div className="artist_view">
-    <div className="artist_background"
-    style={{
-        background:`url(/images/${data.artistData[0].cover})`
-    }}
-    >
-    <Link>
-    Back Home
-    </Link>
-    </div>
-    </div>
-    : null
-)
-
-
-    render(){
-        console.log(this.props)
-        return(
-            <>
-              {this.artistTemplate(this.props.artist)}
-            </>
-        )
-    }
+function mapStateToProps(state) {
+  return {
+    artistData: state.artist,
+  };
 }
 
-function mapStateToProps(state){
-    return {
-        artistData: state.artist
-    }
-} 
-
-export default connect( mapStateToProps)(Artist);
+export default connect(mapStateToProps)(Artist);
